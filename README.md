4882647a648fc88507f6efcb54f5ba66bb064ee6
ata-bayraktar-appcent <ata.bayraktar@appcent.mobi>
2024-01-05 01:11:08 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# template
-template with Typescript, Next.js, Redux Tool Kit, SASS
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+## Getting Started
+
+First, download packages:
+
+```bash
+npm i
+```
+
+Then, run the development server:
+
+```bash
+npm run dev
+```
+
+Used Tech:
+
+```bash
+Typescript
+Next.js -> routing
+Sass -> styling
+ReduxToolKit -> state management
+Swiper -> carousel components
+```
---
+++ src/pages/campaign/[seoName]/[id].tsx
@@ -0,0 +1,53 @@
+import Head from "next/head";
+import Button from "../../../components/Button";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
+import { getPromotion } from "../../../redux/entity/entity.slice";
+
+export default function Campaign() {
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+  const { id } = router.query;
+
+  useEffect(() => {
+    if (dispatch && router.query.id !== undefined) {
+      dispatch(getPromotion(id));
+    }
+  }, [id]);
+
+  const { promotion } = useAppSelector((state) => state.promotion);
+
+  return (
+    <>
+      <Head>
+        <title>Promosyon Detayları</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="page-wrapper">
+        <div className="content-wrapper">
+          <div className="details">
+            <div className="details-image">
+              <img src={promotion.ImageUrl} />
+              <div className="details-logo">
+                <img src={promotion.BrandIconUrl} />
+              </div>
+              <div className="details-due-date">{promotion.RemainingText}</div>
+            </div>
+            <div
+              className="details-title"
+              dangerouslySetInnerHTML={{ __html: promotion.Title }}
+            />
+            <div
+              className="details-description"
+              dangerouslySetInnerHTML={{ __html: promotion.Description }}
+            />
+            <div className="details-button">
+              <Button variant="join-now " label="Hemen Katıl" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
